a9c6ecdcdb855f12397c8a7bebf2131158affc1e
LarissaAngioni <larissaangioni@gmail.com>
2023-07-11 15:06:29 -0300

    Desafio Pontos no Campeonato finalizado.

---
+++ Pontos no campeonato/index.js
@@ -0,0 +1,11 @@
+const resultados = ["V", "E", "V", "E"];
+let pontos = 0;
+
+for (let resultado of resultados) {
+    if (resultado === "V") {
+        pontos += 3;
+    } else if (resultado === "E") {
+        pontos++;
+    }
+}
+console.log(pontos);
